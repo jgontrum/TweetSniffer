a7744925fb1d954a7f1fa029975118ae8e81cfd0
Johannes Gontrum <gontrum@uni-potsdam.de>
2014-10-22 10:58:35 +0200

    First commit

---
+++ src/main/java/de/up/ling/stud/TweetSniffer/App.java
@@ -0,0 +1,13 @@
+package de.up.ling.stud.TweetSniffer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
