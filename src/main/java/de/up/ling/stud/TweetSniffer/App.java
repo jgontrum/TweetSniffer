a9613c1a59106af8de8508d22cb6aa93143754c6
Johannes Gontrum <gontrum@uni-potsdam.de>
2014-11-12 19:31:22 +0100

    Using lambda expression for streaming now.

--- src/main/java/de/up/ling/stud/TweetSniffer/App.java
+++ src/main/java/de/up/ling/stud/TweetSniffer/App.java
@@ -5,31 +5,42 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws InterruptedException, FileNotFoundException, IOException
     {
+        // Load the four API keys from a file
+        String[] apiKeys = loadAPISettings("config.properties");
+        
+        // Build a list of stopwords or user ids to track.
+        // Just leave the list empty if you do not want specific terms / users.
+        // This should be read from file as well. // TODO
+        List<Long> users = Lists.newArrayList(1234L, 566788L);
+        List<String> stopwords = Lists.newArrayList("twitter", "api");
+        
+        // Start streaming.
+        TweetStreamer.stream(apiKeys, stopwords, users, status -> {
+            System.err.println(status.getText());
+        });
+    }
+    
+    private static String[] loadAPISettings(String file) throws IOException  {
         // Load API keys from property-file
         Properties prop = new Properties();
-        InputStream input = new FileInputStream("config.properties");
+        InputStream input = new FileInputStream(file);
         prop.load(input);
-        
+
         String[] apiKeys = new String[4];
-        
+
         apiKeys[0] = prop.getProperty("consumerKey", "");
         apiKeys[1] = prop.getProperty("consumerSecret", "");
         apiKeys[2] = prop.getProperty("token", "");
         apiKeys[3] = prop.getProperty("secret", "");
-        
+
         System.err.println(Arrays.toString(apiKeys));
 
         if (apiKeys[0].isEmpty() || apiKeys[1].isEmpty() || apiKeys[2].isEmpty() || apiKeys[3].isEmpty()) {
@@ -37,17 +48,6 @@ public class App
             System.exit(-1);
         }
         
-        // Build a list of stopwords or user ids to track.
-        // Just leave the list empty if you do not want specific terms / users.
-        List<Long> users = Lists.newArrayList(1234L, 566788L);
-        List<String> stopwords = Lists.newArrayList("twitter", "api");
-        
-        // Initialize the streamer
-        TweetStreamer streamer = new TweetStreamer(apiKeys, stopwords, users);
-        
-        streamer.startStreaming(status -> {
-            System.err.println(status.getText());
-        });
-
+        return apiKeys;
     }
 }
--- src/main/java/de/up/ling/stud/TweetSniffer/TweetStreamer.java
+++ src/main/java/de/up/ling/stud/TweetSniffer/TweetStreamer.java
@@ -5,8 +5,10 @@
  */
 package de.up.ling.stud.TweetSniffer;
 
+import com.google.common.primitives.Longs;
 import java.util.List;
 import java.util.function.Consumer;
+import twitter4j.FilterQuery;
 import twitter4j.Status;
 import twitter4j.StatusListener;
 import twitter4j.TwitterStream;
@@ -14,46 +16,53 @@ import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
- *
+ * Wrapper for all methods to connect to the Twitter streaming API.
+ * Just call the startStreaming method with a function object and let 
+ * the fun begin! 
+ * Have a look at the JavaDocs for more details:
+ * http://twitter4j.org/javadoc/twitter4j/
+ * 
  * @author Johannes Gontrum <gontrum@uni-potsdam.de>
  */
 public class TweetStreamer {
-    private final List<String> stopwordList;
-    private final List<Long> userList;
-    private final ConfigurationBuilder config;
-
-    public TweetStreamer(String[] apiKeys, List<String> stopwords, List<Long> users ) throws InterruptedException {
+    public static void stream(String[] apiKeys, List<String> stopwords, List<Long> users, Consumer<Status> fn) throws InterruptedException {
         // Save the arguments
         String consumerKey = apiKeys[0];
         String consumerSecret = apiKeys[1];
         String token = apiKeys[2];
         String secret = apiKeys[3];
         
-        stopwordList = stopwords;
-        userList = users;
+        List<String> stopwordList = stopwords;
+        List<Long> userList = users;
         
         // Store the configuration
-        config = new ConfigurationBuilder();
+        ConfigurationBuilder config = new ConfigurationBuilder();
         config.setDebugEnabled(true);
         config.setOAuthConsumerKey(consumerKey);
         config.setOAuthConsumerSecret(consumerSecret);
         config.setOAuthAccessToken(token);
         config.setOAuthAccessTokenSecret(secret);
-    }   
-    
-    public void startStreaming(Consumer<Status> fn) {
+        
         // Create a status listener. When a new tweet arives, the function object will accept it.
         StatusListener listener = new TwitterStatusListener(fn);
 
+        // Create the filter. Can filter:
+        //  * by Users (follow[])
+        //  * by Stopword (track[])
+        //  * by Location (locations[][])
+        //  * by Language (language[])
+        FilterQuery filter = new FilterQuery();
+        filter.track(stopwordList.toArray(new String[stopwordList.size()]));
+        filter.follow(Longs.toArray(userList));
+        
         // Create the stream-object
+        // Documentation: http://twitter4j.org/javadoc/twitter4j/TwitterStream.html
         TwitterStream stream = new TwitterStreamFactory(config.build()).getInstance();
 
         stream.addListener(listener);
+        stream.filter(filter);
         
+        // Start streaming!
         stream.sample();
     }
-      
-   
-    
-   
 }
