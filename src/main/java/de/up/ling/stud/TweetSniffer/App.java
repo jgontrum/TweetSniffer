7eb682efa2ef96bd1985318114c0f5126d4bb28b
Johannes Gontrum <gontrum@uni-potsdam.de>
2014-11-13 20:48:03 +0100

    Started to work on mySQL connection

--- src/main/java/de/up/ling/stud/TweetSniffer/App.java
+++ src/main/java/de/up/ling/stud/TweetSniffer/App.java
@@ -13,8 +13,13 @@ public class App
 {
     public static void main( String[] args ) throws InterruptedException, FileNotFoundException, IOException
     {
+        String configFile = "config.properties";
         // Load the four API keys from a file
-        String[] apiKeys = loadAPISettings("config.properties");
+        String[] apiKeys = loadAPISettings(configFile);
+        String[] sqlSettings = loadSQLSettings(configFile);
+        
+        // Connect do database...
+        MySQLAccess database = new MySQLAccess(sqlSettings);
         
         // Build a list of stopwords or user ids to track.
         // Just leave the list empty if you do not want specific terms / users.
@@ -22,10 +27,12 @@ public class App
         List<Long> users = Lists.newArrayList();
         List<String> stopWords = Lists.newArrayList("squirrel");
         
+        /*
         // Start streaming.
         TweetStreamer.stream(apiKeys, stopWords, users, tweet -> {
             System.err.println(tweet.getText());
         });
+        */
     }
     
     private static String[] loadAPISettings(String file) throws IOException  {
@@ -41,8 +48,6 @@ public class App
         apiKeys[2] = prop.getProperty("token", "");
         apiKeys[3] = prop.getProperty("secret", "");
 
-        System.err.println(Arrays.toString(apiKeys));
-
         if (apiKeys[0].isEmpty() || apiKeys[1].isEmpty() || apiKeys[2].isEmpty() || apiKeys[3].isEmpty()) {
             System.err.println("Please specify your consumerKey, consumerSecret, token and secret in a file called 'config.properties");
             System.exit(-1);
@@ -50,4 +55,23 @@ public class App
         
         return apiKeys;
     }
+
+    private static String[] loadSQLSettings(String configFile) throws IOException {
+        // Load SQL settings from property-file
+        Properties prop = new Properties();
+        InputStream input = new FileInputStream(configFile);
+        prop.load(input);
+
+        String[] sqlSettings = new String[3];
+
+        sqlSettings[0] = prop.getProperty("mySQLHost", "");
+        sqlSettings[1] = prop.getProperty("mySQLUser", "");
+        sqlSettings[2] = prop.getProperty("mySQLPassword", "");
+
+        if (sqlSettings[0].isEmpty() || sqlSettings[1].isEmpty() || sqlSettings[2].isEmpty()) {
+            System.err.println("Please specify your mySQLHost, mySQLUser and mySQLPassword in a file called 'config.properties");
+            System.exit(-1);
+        }
+
+        return sqlSettings;    }
 }
